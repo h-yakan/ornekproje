9233990ced2c5e4ad48a8bead4bf34e61a6a5c48
Hüseyin Yakan <hyakan@hotmail.com>
2023-07-24 14:38:52 +0300

    commit

--- kurslar/urls.py
+++ kurslar/urls.py
@@ -2,9 +2,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('',views.home),
-    path('anasayfa',views.home),
+    path('',views.kurslar),
     path('<kurs_adi>',views.getAsd),
     path('kategori/<int:category_id>',views.getByCategoryNum),
-    path('kategori/<str:category_name>',views.getByCategoryName),
+    path('kategori/<str:category_name>',views.getByCategoryName,name = 'courses_by_category'),
 ]
