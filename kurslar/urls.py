5cb0644fdb13290eae1197d6b4fed2f531129c3e
Hüseyin Yakan <hyakan@hotmail.com>
2023-07-26 10:20:04 +0300

    commit

--- kurslar/urls.py
+++ kurslar/urls.py
@@ -3,7 +3,8 @@ from . import views
 
 urlpatterns = [
     path('',views.index),
-    path('<kurs_adi>',views.getAsd),
     path('kategori/<int:category_id>',views.getByCategoryNum),
     path('kategori/<str:category_name>',views.getByCategoryName,name = 'courses_by_category'),
+    path('<kurs_adi>',views.getAsd),
+    
 ]
