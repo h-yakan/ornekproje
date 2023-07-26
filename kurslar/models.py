38bfabc479021ea3965763a62255b133d0d342cd
Hüseyin Yakan <hyakan@hotmail.com>
2023-07-26 11:59:49 +0300

    commit

--- kurslar/models.py
+++ kurslar/models.py
@@ -1,5 +1,9 @@
 from django.db import models
 
+class Kategoriler(models.Model):
+    name = models.CharField(max_length=30)
+    slug = models.SlugField(max_length=15)
+
 class Kurs(models.Model):
     title = models.CharField(max_length=30)
     description = models.TextField(null=True)
@@ -7,8 +11,3 @@ class Kurs(models.Model):
     slug = models.SlugField(max_length=15)
     date = models.DateField()
     isActive = models.BooleanField()
-
-class Kategoriler(models.Model):
-    name = models.CharField(max_length=30)
-    slug = models.SlugField(max_length=15)
-    
