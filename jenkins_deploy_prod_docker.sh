3213448cc77a7afd9037df8309def5a5d910b036
Hüseyin Yakan <140379978+h-yakan@users.noreply.github.com>
2023-11-24 17:41:21 +0300

    Update jenkins_deploy_prod_docker.sh

--- jenkins_deploy_prod_docker.sh
+++ jenkins_deploy_prod_docker.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
-ssh root@YOUR_SERVER_IP_ADDRESS <<EOF
-  cd /path/to/your/project
+ssh ubuntu@ec2-13-48-190-187.eu-north-1.compute.amazonaws.com <<EOF
+  cd /repo/ornekproje
   git pull
   docker-compose -f docker-compose.prod.yml up --build -d
   exit
