e082d0e28d9ed5387cb440a7efd8f1bdc9429111
Hüseyin Yakan <140379978+h-yakan@users.noreply.github.com>
2023-11-27 13:27:44 +0300

    Update jenkins_deploy_prod_docker.sh

--- jenkins_deploy_prod_docker.sh
+++ jenkins_deploy_prod_docker.sh
@@ -1,8 +1,1 @@
-#!/bin/sh
 
-ssh -i /home/user/.ssh/UrlshortenerV1.pem ubuntu@ec2-13-48-190-187.eu-north-1.compute.amazonaws.com <<EOF
-  cd /repo/ornekproje
-  git pull
-  docker-compose -f docker-compose.prod.yml up --build -d
-  exit
-EOF
