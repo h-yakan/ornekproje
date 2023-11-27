c46c863e04dafa3ac16518d3c5af20017895c192
Hüseyin Yakan <140379978+h-yakan@users.noreply.github.com>
2023-11-27 14:03:44 +0300

    Update settings.py

--- ornekproje/settings.py
+++ ornekproje/settings.py
@@ -78,9 +78,13 @@ WSGI_APPLICATION = 'ornekproje.wsgi.application'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+    'default': {        
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": "hello_django_prod",
+        "USER": "hello_django",
+        "PASSWORD": "hello_django",
+        "HOST": "db",
+        "PORT": 5432,
     }
 }
 
